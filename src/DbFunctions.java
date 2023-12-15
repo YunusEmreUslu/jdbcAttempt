096cf030421bcece664e426adbc48ff4d698849e
YunusEmreUslu <usluyunusemre00@gmail.com>
2023-12-15 17:15:23 +0300

    made createTable function

--- src/DbFunctions.java
+++ src/DbFunctions.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DbFunctions {
     public Connection connect_to_db(String dbname, String user, String pass){
@@ -18,5 +19,16 @@ public class DbFunctions {
             System.out.println(e);
         }
         return conn;
+    }
+    public void createTable(Connection conn, String table_name){
+        Statement statement;
+        try{
+            String query="create table "+table_name+"(empid SERIAL,name varchar(200), address varchar(200),primary key(empid));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table created");
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
+import java.sql.Connection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         DbFunctions db=new DbFunctions();
-        db.connect_to_db("tutdb","postgres","usluneinnein99");
-
+        Connection conn=db.connect_to_db("tutdb","postgres","usluneinnein99");
+        db.createTable(conn, "employee");
     }
 }
